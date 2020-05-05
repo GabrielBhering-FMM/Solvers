90e7c2bbbddf44f2ca0c76184a92456a88d4e17a
gabe <60207467+YstjawdTzwjafy@users.noreply.github.com>
2020-05-05 11:33:53 -0400

    Tela de login e cadastro

--- SolversApp/app/src/main/java/com/example/solvers/MainActivity.java
+++ SolversApp/app/src/main/java/com/example/solvers/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.solvers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.CDATASection;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+    private FirebaseAuth mAuth;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        Button btLogin = (Button) findViewById(R.id.btLogin);
+        Button btCad = (Button) findViewById(R.id.btLogin2);
+
+        btLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextView txtUser = (TextView) findViewById(R.id.txtUser);
+                TextView txtPw = (TextView) findViewById(R.id.txtPw);
+                String email = txtUser.getText().toString();
+                String pw = txtPw.getText().toString();
+                if(email.equals(RegisterActivity.email_geral) && pw.equals(RegisterActivity.senha_geral)){
+                    alert("Logou!");
+                }
+                else{
+                    alert("Não logou :(");
+                }
+
+            }
+        });
+
+        btCad.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                LaunchActivity();
+            }
+        });
+
+
     }
+    private void alert(String s){
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+    }
+    public void LaunchActivity(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+
+    }
+
 }
---
+++ SolversApp/app/src/main/java/com/example/solvers/RegisterActivity.java
@@ -0,0 +1,55 @@
+package com.example.solvers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+    public static String username_geral;
+    public static String email_geral;
+    public static String senha_geral;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button btCadastrarUser = (Button) findViewById(R.id.btCadastrarUser);
+        btCadastrarUser.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                TextView txtNome = (TextView) findViewById(R.id.txtUsername);
+                TextView txtEmail = (TextView) findViewById(R.id.txtEmail);
+                TextView txtSenha = (TextView) findViewById(R.id.pwUser);
+                TextView txtSenhaDnv = (TextView) findViewById(R.id.pwUserDnv);
+
+                String NomeCad = txtNome.getText().toString();
+                String EmailCad = txtEmail.getText().toString();
+                String PwCad = txtSenha.getText().toString();
+                String PwDnvCad = txtSenhaDnv.getText().toString();
+
+                if(PwCad.equals(PwDnvCad)){
+                    username_geral = NomeCad;
+                    email_geral = EmailCad;
+                    senha_geral = PwCad;
+                    LaunchActivity();
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Erro no Cadastro",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+            private void LaunchActivity() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
