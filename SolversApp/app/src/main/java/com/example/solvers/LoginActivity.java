da5e856e20c13e8ecb0cff5c4912c30f6652b331
Ph-FMM <pedroaraujo1952@hotmail.com>
2020-09-04 00:20:01 -0400

    Login Screen

---
+++ SolversApp/app/src/main/java/com/example/solvers/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.solvers;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
